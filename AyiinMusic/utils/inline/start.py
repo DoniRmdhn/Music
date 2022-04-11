1dc17569f027c64436361ed0e4b0cb7cd0e411bc
DoniRmdhn <103444731+DoniRmdhn@users.noreply.github.com>
2022-04-12 05:57:26 +0700

    Update start.py

--- AyiinMusic/utils/inline/start.py
+++ AyiinMusic/utils/inline/start.py
@@ -106,9 +106,6 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(
-                    text=_["S_B_6"], url=f"{GITHUB_REPO}"
-                ),
             ]
         )
     else:
@@ -116,7 +113,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             buttons.append(
                 [
                     InlineKeyboardButton(
-                        text=_["S_B_6"], url=f"{GITHUB_REPO}"
+                        text=_["S_B_7"], user_id=OWNER
                     ),
                 ]
             )
